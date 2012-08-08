a48d840266473f9ac78aa989ce9e5c72ae71b1f0
Progi1984 <progi1984@gmail.com>
2012-08-08 21:16:04 +0200

    IMPROVED : Support du mode offline en JavaScript

--- article/html5/offline/script.js
+++ article/html5/offline/script.js
@@ -1,43 +1,63 @@
+ var bOnlineStatus;
+ 
  $(document).ready(function(){
+	bOnlineStatus = navigator.onLine;
 	$('#frmTodo').submit(function(){
-		var sVal = $('#txtItem').val();
-		var sVal = $.trim(sVal);
-		if(sVal != ''){
+		if(bOnlineStatus){
+			var sVal = $('#txtItem').val();
+			var sVal = $.trim(sVal);
+			if(sVal != ''){
+				$.ajax({
+					url: 'ajax.php',
+					dataType: 'json',
+					type: 'POST',
+					data:{
+						type:'add',
+						item:sVal
+					},
+					success: function(data) {
+						if(data.result == true){
+							var sItem = '<li><span>'+sVal+'</span><img src="delete.png" alt="Supprimer" class="delTask"/></li>';
+							$('ul').append(sItem);
+							$('#txtItem').val('');
+						}
+					}
+				});
+				
+			}
+		} else {
+			alert('Reseau : Offline');
+		}
+		return false;
+	});
+	$(document).on('click', '.delTask', function(){
+		if(bOnlineStatus){
+			var jqoParent = $(this).parent();
 			$.ajax({
 				url: 'ajax.php',
 				dataType: 'json',
 				type: 'POST',
 				data:{
-					type:'add',
-					item:sVal
+					type:'del',
+					pos:$('li').index(jqoParent)
 				},
 				success: function(data) {
 					if(data.result == true){
-						var sItem = '<li><span>'+sVal+'</span><img src="delete.png" alt="Supprimer" class="delTask"/></li>';
-						$('ul').append(sItem);
-						$('#txtItem').val('');
+						jqoParent.remove();
 					}
 				}
 			});
-			
+		} else {
+			alert('Reseau : Offline');
 		}
-		return false;
-	});
-	$(document).on('click', '.delTask', function(){
-		var jqoParent = $(this).parent();
-		$.ajax({
-			url: 'ajax.php',
-			dataType: 'json',
-			type: 'POST',
-			data:{
-				type:'del',
-				pos:$('li').index(jqoParent)
-			},
-			success: function(data) {
-				if(data.result == true){
-					jqoParent.remove();
-				}
-			}
-		});
 	});
  });
+ 
+window.addEventListener('online', function() {
+	bOnlineStatus = false;
+}, true);
+ 
+window.addEventListener('offline', function() {
+	bOnlineStatus = false;
+	alert('Vous etes maintenant hors-ligne. Vos taches ne seront pas enregistres');
+}, true);
