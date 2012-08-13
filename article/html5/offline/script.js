3c2b141a64b30dfc645156afa9c904d5aedf31e6
Progi1984 <progi1984@gmail.com>
2012-08-13 22:19:52 +0200

    IMPROVED : Commentaire dans le JS

--- article/html5/offline/script.js
+++ article/html5/offline/script.js
@@ -2,11 +2,16 @@
  
  $(document).ready(function(){
 	bOnlineStatus = navigator.onLine;
+	
+	// Evenement lors de la submission du formulaire
 	$('#frmTodo').submit(function(){
+		// Si on est en ligne
 		if(bOnlineStatus){
+			// On récupére le text nettoyé des espaces avant et après
 			var sVal = $('#txtItem').val();
 			var sVal = $.trim(sVal);
 			if(sVal != ''){
+				// On envoie une requête AJAX de type POST pour ajouter la tache
 				$.ajax({
 					url: 'ajax.php',
 					dataType: 'json',
@@ -25,14 +30,18 @@
 				});
 				
 			}
-		} else {
+		} 
+		// Si on n'est pas en ligne
+		else {
 			alert('Reseau : Offline');
 		}
 		return false;
 	});
 	$(document).on('click', '.delTask', function(){
+		// Si on est en ligne
 		if(bOnlineStatus){
 			var jqoParent = $(this).parent();
+			// On envoie une requête AJAX de type POST pour supprimer la tache
 			$.ajax({
 				url: 'ajax.php',
 				dataType: 'json',
@@ -47,7 +56,9 @@
 					}
 				}
 			});
-		} else {
+		} 
+		// Si on n'est pas en ligne
+		else {
 			alert('Reseau : Offline');
 		}
 	});
